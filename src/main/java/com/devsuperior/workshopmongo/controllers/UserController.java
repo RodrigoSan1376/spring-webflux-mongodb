fb8e1e4153892f493a5d7e1e27f7ba455be5a3e8
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-05-20 22:08:52 -0300

    GET all users, user by id

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -1,10 +1,17 @@
 package com.devsuperior.workshopmongo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.services.UserService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -12,6 +19,17 @@ public class UserController {
 
 	@Autowired
 	private UserService service;
+	
+	@GetMapping
+	public Flux<UserDTO> findAll() {
+		return service.findAll();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Mono<ResponseEntity<UserDTO>> findById(@PathVariable String id) {
+		return service.findById(id).map(userDto -> ResponseEntity.ok().body(userDto));
+	}
+	
 	/*
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -3,13 +3,30 @@ package com.devsuperior.workshopmongo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
+import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository repository;	
+	
+	public Flux<UserDTO> findAll() {
+		return repository.findAll().map(user -> new UserDTO(user));
+	}
+	
+	public Mono<UserDTO> findById(String id) {
+		return repository.findById(id)
+				.map(existingUser -> new UserDTO(existingUser))
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado")));
+	}
+	
+	
 	/*
 	@Transactional(readOnly = true)
 	public List<UserDTO> findAll() {
