9b606583350cb8aedf5b2d6801a7256535aa973e
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-05-21 21:05:28 -0300

    GET post by ID

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,10 +1,16 @@
 package com.devsuperior.workshopmongo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.services.PostService;
+
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -12,6 +18,12 @@ public class PostController {
 
 	@Autowired
 	private PostService service;
+	
+	@GetMapping(value = "/{id}")
+	public Mono<ResponseEntity<PostDTO>> findById(@PathVariable String id) {
+		return service.findById(id).map(postDto -> ResponseEntity.ok().body(postDto));
+	}
+	
 	/*
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
