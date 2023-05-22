37788c242eaaa683faffc8cb7d34961d3c217061
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-05-21 21:31:50 -0300

    GET post by title

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -1,15 +1,21 @@
 package com.devsuperior.workshopmongo.controllers;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.workshopmongo.controllers.util.URL;
 import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.services.PostService;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -24,7 +30,13 @@ public class PostController {
 		return service.findById(id).map(postDto -> ResponseEntity.ok().body(postDto));
 	}
 	
-	/*
+	@GetMapping(value = "/titlesearch")
+	public Flux<PostDTO> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+		text = URL.decodeParam(text);
+		return service.findByTitle(text);
+	}
+	
+	/* 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
 		PostDTO dto = service.findById(id);
